bde8caab3775890b7ff224bb5f3727c519cd0bb9
Raul Menendez <rmenendezm@gmail.com>
2018-10-10 11:09:12 -0400

    model updated

--- cargo/models.py
+++ cargo/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse #Used to generate urls by reversing the URL patt
 from address.models import AddressField
 from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
+from djmoney.models.fields import MoneyField
 
 # Create your models here.
 
@@ -11,13 +12,13 @@ class EmployeeRole(models.Model):
     """
     Model representing an employee role (e.g. broker, dispatcher, driver, manager,owner).
     """
-    role = models.CharField(max_length=200, unique=True, help_text="Enter an employee role (e.g. broker, dispatcher, driver, manager, owner)")
+    name = models.CharField(max_length=200, unique=True, help_text="Enter an employee role (e.g. broker, dispatcher, driver, manager, owner)")
     
     def __str__(self):
         """
         String for representing the Model object (in Admin site etc.)
         """
-        return self.role
+        return self.name
         
         
 class CompanyType(models.Model):
@@ -73,7 +74,7 @@ class Person(models.Model):
     """
     Model representing a Person (e.g. Lolo Perez, etc.)
     """
-    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.PROTECT, null=True, blank=True)
     cell = PhoneNumberField(blank=True, help_text="Enter the person contact number (e.g. +19999999999, etc.)")
     confirmed_phone = models.BooleanField(default=False)
 
@@ -88,15 +89,77 @@ class Employee(models.Model):
     """
     Model representing an Employee (e.g. Lolo Perez, dispatcher at Galianos Corp.)
     """
-    person = models.OneToOneField(Person, on_delete=models.PROTECT)
+    person = models.ForeignKey(Person, on_delete=models.PROTECT, help_text="Select the person this employee represents")
     company = models.ForeignKey(Company, on_delete=models.PROTECT, help_text="Select the company this employee works in")
-    role = models.ForeignKey(EmployeeRole, on_delete=models.PROTECT, help_text="Select the role this employee have in the company"
-    
+    role = models.ManyToManyField(EmployeeRole, on_delete=models.PROTECT, help_text="Select the roles this employee have in the company")
+
+    class Meta:
+        ordering = ["company","person"]
+        #permissions = (("can_edit_book", "Allowed to edit"),)   
+
+    def display_role(self):
+        """
+        Creates a string for the Role. This is required to display role in Admin.
+        """
+        return ', '.join([ role.name for role in self.role.all()[:3] ])
+        
+    display_role.short_description = 'Role XXXX'
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular employee
+        """
+        return reverse('employee-detail', args=[str(self.id)])
+
     def __str__(self):
         """
         String for representing the Model object (in Admin site etc.)
         """
-        return self.user.get_full_name()
+        return '{0} ({1})'.format(self.user.get_full_name(), self.company.name) 
+
+
+class Cargo(models.Model):
+    """
+    Model representing a Cargo or Load (e.g. Shipment x from  Broker y)
+    """
+    description = models.CharField(max_length=200, help_text="Enter a description for the cargo")
+    price = MoneyField(max_digits=14, decimal_places=2, default_currency='USD')
+    broker = models.ForeignKey(Employee, on_delete=models.PROTECT, help_text="Represents the employee from a brokerage company posting the cargo")
+    posted = models.DateTimeField(auto_now_add=True, blank=False, help_text="Represents a timestamp of when the cargo was posted" )
+    
+    CARGO_STATUS = (
+        ('p', 'Posted'),
+        ('n', 'Negotiated'),
+        ('a', 'Assigned'),
+        ('o', 'On route'),
+        ('d', 'Delivered'),
+    )
+
+    status= models.CharField(max_length=1, choices=CARGO_STATUS, default='p', help_text='Cargo status')
+
+    dispatcher = models.ForeignKey(Employee, on_delete=models.PROTECT, help_text="Represents the employee from a carrier company who close the deal with the broker")
+    negotiated = models.DateTimeField(blank=True, help_text="Represents a timestamp of when the cargo was negotiated with the broker" )
+    
+    driver = models.ForeignKey(Employee, on_delete=models.PROTECT, help_text="Represents the employee from a carrier company who was assigned for delivering the cargo")
+    assigned = models.DateTimeField(blank=True, help_text="Represents a timestamp of when the cargo was assigned to the driver" )
+    
+    delivered = models.DateTimeField(blank=True, help_text="Represents a timestamp of when the cargo was delivered" )
+
+    class Meta:
+        ordering = ['-posted','description']
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular cargo instance.
+        """
+        return reverse('cargo-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return '{0} - {1} - {2} - {3}'.format(self.posted, self.description, self.price, self.broker.company.name) 
+    
 
 class Lumper(models.Model):
 
@@ -107,112 +170,8 @@ class Advancement(models.Model):
 class Check(models.Model):
 
 
-class Cargo(models.Model):
+
 
 
 class PickupOrder(models.Model):
 
-    
-# class Book(models.Model):
-#     """
-#     Model representing a book (but not a specific copy of a book).
-#     """
-#     title = models.CharField(max_length=200)
-#     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-#       # Foreign Key used because book can only have one author, but authors can have multiple books
-#       # Author as a string rather than object because it hasn't been declared yet in file.
-#     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
-#     isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-#     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
-#       # ManyToManyField used because a genre can contain many books and a Book can cover many genres.
-#       # Genre class has already been defined so we can specify the object above.
-#     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
-      
-#     def display_genre(self):
-#         """
-#         Creates a string for the Genre. This is required to display genre in Admin.
-#         """
-#         return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
-#         display_genre.short_description = 'Genre'
-    
-    
-#     def get_absolute_url(self):
-#         """
-#         Returns the url to access a particular book instance.
-#         """
-#         return reverse('book-detail', args=[str(self.id)])
-
-#     def __str__(self):
-#         """
-#         String for representing the Model object.
-#         """
-#         return self.title
-        
-        
-# import uuid # Required for unique book instances
-# from datetime import date
-
-# from django.contrib.auth.models import User #Required to assign User as a borrower
-
-# class BookInstance(models.Model):
-#     """
-#     Model representing a specific copy of a book (i.e. that can be borrowed from the library).
-#     """
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
-#     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
-#     imprint = models.CharField(max_length=200)
-#     due_back = models.DateField(null=True, blank=True)
-#     borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    
-#     @property
-#     def is_overdue(self):
-#         if self.due_back and date.today() > self.due_back:
-#             return True
-#         return False
-        
-
-#     LOAN_STATUS = (
-#         ('d', 'Maintenance'),
-#         ('o', 'On loan'),
-#         ('a', 'Available'),
-#         ('r', 'Reserved'),
-#     )
-
-#     status= models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='d', help_text='Book availability')
-
-#     class Meta:
-#         ordering = ["due_back"]
-#         permissions = (("can_mark_returned", "Set book as returned"),)   
-
-#     def __str__(self):
-#         """
-#         String for representing the Model object.
-#         """
-#         #return '%s (%s)' % (self.id,self.book.title)
-#         return '{0} ({1})'.format(self.id,self.book.title)
-        
-
-# class Author(models.Model):
-#     """
-#     Model representing an author.
-#     """
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#     date_of_birth = models.DateField(null=True, blank=True)
-#     date_of_death = models.DateField('died', null=True, blank=True)
-
-#     class Meta:
-#         ordering = ["last_name","first_name"]
-    
-#     def get_absolute_url(self):
-#         """
-#         Returns the url to access a particular author instance.
-#         """
-#         return reverse('author-detail', args=[str(self.id)])
-    
-
-#     def __str__(self):
-#         """
-#         String for representing the Model object.
-#         """
-#         return '{0}, {1}'.format(self.last_name,self.first_name)
--- cargomonitoring/settings.py
+++ cargomonitoring/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'address',
+    'djmoney',
     'phonenumber_field',
     'cargo.apps.CargoConfig', 
 ]
